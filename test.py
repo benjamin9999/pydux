1346d25b58f646dd58e7f1492bbdba2163294077
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-10-20 19:40:23 +0100

    Add test for reducer application

--- test.py
+++ test.py
@@ -10,9 +10,19 @@ def id(state=[]):
     return reduce(f, state, 0) + 1
 
 def reducer_todos(state, action):
+    if state is None:
+        state = []
     if action['type'] == 'ADD_TODO':
-        return state[:].append({id: id(state), text: action['text']})
+        copy = state[:]
+        copy.append({'id': id(state), 'text': action['text']})
+        return copy
     return state
+
+def unknown_action():
+    return {'type': 'UNKNOWN_ACTION'}
+
+def add_todo(text):
+    return {'type': 'ADD_TODO', 'text': text}
 
 class TestCreateStore(unittest.TestCase):
     def test_exposes_public_api(self):
@@ -38,6 +48,34 @@ class TestCreateStore(unittest.TestCase):
         ])
         self.assertEqual(store.get_state(), [
             {'id': 1, 'text': 'Hello'}
+        ])
+
+    def test_applies_reducer(self):
+        '''applies the reducer to the previous state'''
+        store = pydux.create_store(reducer_todos)
+        self.assertEqual(store.get_state(), [])
+
+        store.dispatch(unknown_action())
+        self.assertEqual(store.get_state(), [])
+
+        store.dispatch(add_todo('Hello'))
+        self.assertEqual(store.get_state(), [
+            {
+                'id': 1,
+                'text': 'Hello'
+            }
+        ])
+
+        store.dispatch(add_todo('World'))
+        self.assertEqual(store.get_state(), [
+            {
+                'id': 1,
+                'text': 'Hello'
+            },
+            {
+                'id': 2,
+                'text': 'World'
+            }
         ])
 
 
